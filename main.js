da9c21708887dd8a7168d336712c67986c5949df
weizhen1094 <weizhen1094@gmail.com>
2025-06-20 01:16:53 +0800

    total score: 0

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ function get_cookie(isCookie)
     //score.style.top = getRndInteger(37, 63).toString() + '%'; 
     score.animate([ {opacity: '100%'}], {duration: 570});
     document.body.appendChild(score);
-    setTimeout(function () { document.body.removeChild(score); }, 650)
+    //setTimeout(function () { document.body.removeChild(score); }, 650)
     // update total score
     totalScore = isCookie ? totalScore + 1 : totalScore - 1;
     document.getElementById('total-score-text').innerHTML = 'total score: ' + totalScore.toString();

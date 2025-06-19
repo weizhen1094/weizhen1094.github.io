65fcdeec1d7131887854b57d4965932ac9a3a514
weizhen1094 <weizhen1094@gmail.com>
2025-06-20 00:38:21 +0800

    total score

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+var totalScore = 0;
+
+function getRndInteger(min, max) 
+{ return Math.floor(Math.random() * (max - min) ) + min; }
+
 function get_cookie(isCookie) 
 {
     // display score
@@ -5,5 +10,12 @@ function get_cookie(isCookie)
     score.id = 'score-text'; 
     score.innerHTML   = isCookie ? '+1' : '-1';
     score.style.color = isCookie ? 'black' : 'red';
+    score.style.left = getRndInteger(37, 63).toString() + '%'; // todo pos of cookie
+    //score.style.top = getRndInteger(37, 63).toString() + '%'; 
+    score.animate([ {opacity: '100%'}], {duration: 570});
     document.body.appendChild(score);
+    setTimeout(function () { document.body.removeChild(score); }, 650)
+    // update total score
+    totalScore = isCookie ? totalScore + 1 : totalScore - 1;
+    document.getElementById('total-score-text').innerHTML = 'total score: ' + totalScore.toString();
 }
